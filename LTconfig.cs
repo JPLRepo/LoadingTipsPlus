e47f406a09a593f153e883ea9cdeadbde7bf0804
Jamie Leighton <jamieleighton@bigpond.com>
2016-06-30 17:53:28 +1000

    First version

---
+++ LTconfig.cs
@@ -0,0 +1,42 @@
+﻿/* 
+LoadingTipsPlus by JPLRepo is released under the following license:
+Copyright © Jamie Leighton, 2016 All Rights Reserved
+For more info visit the KSP Forum Thread
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LoadingTipsPlus
+{
+    class LTconfig
+    {
+        private const string configNodeName = "LOADINGTOOLTIPSPLUS";
+
+        public bool Overwrite;
+        public List<string> ToolTips;
+
+        public LTconfig()
+        {
+            Overwrite = false;
+            ToolTips = new List<string>();
+        }
+
+        public void Load(ConfigNode node)
+        {
+            if (node.HasNode(configNodeName)) //Load SFS config nodes
+            {
+                ConfigNode LTsettingsNode = node.GetNode(configNodeName);
+
+                LTsettingsNode.TryGetValue("Overwrite", ref Overwrite);
+                string[] values = LTsettingsNode.GetValuesStartsWith("ToolTip");
+                for (int i = 0; i < values.Length; i++)
+                {
+                    ToolTips.Add(values[i]);
+                }
+            }
+        }
+    }
+}
---
+++ LoadingTips.cs
@@ -0,0 +1,108 @@
+﻿/* 
+LoadingTipsPlus by JPLRepo is released under the following license:
+Copyright © Jamie Leighton, 2016 All Rights Reserved
+For more info visit the KSP Forum Thread
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace LoadingTipsPlus
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class LoadingTips : MonoBehaviour
+    {
+        //private string globalConfigFilename;
+        private ConfigNode globalNode = new ConfigNode();
+        private LTconfig LTconfigvals = new LTconfig();
+        private List<LoadingScreen.LoadingScreenState> LoadingScreens = new List<LoadingScreen.LoadingScreenState>();
+        private List<string> NewToolTipsList = new List<string>();
+        private bool processedToolTips = false;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+            try
+            {
+                string DataPath = ((Application.platform != RuntimePlatform.OSXPlayer) ? Path.Combine(Application.dataPath, "../GameData") : Path.Combine(Application.dataPath, "../../ GameData"));
+                string[] files = Directory.GetFiles(DataPath,"*.ltp",SearchOption.AllDirectories);
+                for (int i = 0; i < files.Length; i++)
+                {
+                    globalNode = ConfigNode.Load(files[i]);
+                    LTconfigvals.Load(globalNode);
+                }
+            }
+            catch (UnauthorizedAccessException UAEx)
+            {
+                Debug.Log(UAEx.Message);
+            }
+            catch (PathTooLongException PathEx)
+            {
+                Debug.Log(PathEx.Message);
+            }
+            if (LTconfigvals.ToolTips.Count == 0)
+            {
+                processedToolTips = true;
+            }
+        }
+
+        private void Update()
+        {
+            if (!processedToolTips && LoadingScreen.Instance != null)
+            {
+                LoadingScreens = LoadingScreen.Instance.Screens;
+                foreach (var screen in LoadingScreens)
+                {
+                    NewToolTipsList.Clear();
+                    if (!LTconfigvals.Overwrite)
+                    {
+                        for (int i = 0; i < screen.tips.Length; i++)
+                        {
+                            NewToolTipsList.Add(screen.tips[i]);
+                        }
+                    }
+                    
+                    for (int i = 0; i < LTconfigvals.ToolTips.Count; i++)
+                    {
+                        NewToolTipsList.Add(LTconfigvals.ToolTips[i]);
+                    }
+
+                    
+                    string[] NewToolTips = new string[NewToolTipsList.Count];
+                    NewToolTips = NewToolTipsList.ToArray();
+                    screen.tips = NewToolTips;
+                }
+                processedToolTips = true;
+            }
+            if (HighLogic.LoadedScene == GameScenes.MAINMENU)
+            {
+                DestroyMe();
+            }
+        }
+
+        private void DestroyMe()
+        {
+            DestroyObject(this);
+        }
+
+        /// <summary>
+        /// Name of the Assembly that is running this MonoBehaviour
+        /// </summary>
+        internal static String AssemblyName
+        { get { return Assembly.GetExecutingAssembly().GetName().Name; } }
+
+        /// <summary>
+        /// Full Path of the executing Assembly
+        /// </summary>
+        internal static String AssemblyLocation
+        { get { return Assembly.GetExecutingAssembly().Location.Replace("\\", "/"); } }
+
+        /// <summary>
+        /// Folder containing the executing Assembly
+        /// </summary>
+        internal static String AssemblyFolder
+        { get { return Path.GetDirectoryName(AssemblyLocation).Replace("\\", "/"); } }
+    }
+}
